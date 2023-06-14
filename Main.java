ac85fb8f2081bfc1c35efb3ce42f6475b747d6e7
Krogie <131395075+Krogie@users.noreply.github.com>
2023-06-14 23:07:38 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,17 +6,17 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Erstelle eine Instanz des MOTDChanger-Plugins
+        // Erstelle eine Instanz des MOTDChangers
         motdChanger = new MOTDChanger();
 
-        // Registriere das MOTDChanger-Plugin als Bukkit-Plugin
+        // Registriere den MOTDChanger als Bukkit-Plugin
         getServer().getPluginManager().registerEvents(motdChanger, this);
         getCommand("motd").setExecutor(motdChanger);
     }
 
     @Override
     public void onDisable() {
-        // Deaktiviere das MOTDChanger-Plugin
+        // Deaktiviere den MOTDChanger
         motdChanger = null;
     }
 }
